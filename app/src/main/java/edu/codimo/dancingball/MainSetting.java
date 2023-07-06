7935fca10740402197bff19e848e59e51fda607b
Luis Martinez <luis.hernando.martinez@correounivalle.edu.co>
2023-07-06 00:08:32 -0500

    Gui setting medio avanzada

--- app/src/main/java/edu/codimo/dancingball/MainSetting.java
+++ app/src/main/java/edu/codimo/dancingball/MainSetting.java
@@ -5,6 +5,7 @@ import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainSetting extends AppCompatActivity {
@@ -13,5 +14,9 @@ public class MainSetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_setting);
+    }
+
+    public void backtomainsetting(View view) {
+        finish();
     }
 }
