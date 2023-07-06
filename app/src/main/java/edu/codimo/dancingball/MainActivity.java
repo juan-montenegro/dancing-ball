972978be23976964f11b994e45e4b1ce6d6a7980
Luis Martinez <luis.hernando.martinez@correounivalle.edu.co>
2023-07-05 20:16:55 -0500

    Gui medio avanzada

--- app/src/main/java/edu/codimo/dancingball/MainActivity.java
+++ app/src/main/java/edu/codimo/dancingball/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         WindowInsetsControllerCompat windowInsetsController =
                 WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
         // Configure the behavior of the hidden system bars.
@@ -22,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+    }
+    public void closeApplicationBtn(View view) {
+        finishAffinity(); // Cierra todas las actividades abiertas en la aplicación
+        System.exit(0); // Finaliza el proceso de la aplicación
+    }
 
+    public void opennsettingBtn(View view) {
+        Intent intent = new Intent(this, MainSetting.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/codimo/dancingball/MainSetting.java
@@ -0,0 +1,17 @@
+package edu.codimo.dancingball;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_setting);
+    }
+}
